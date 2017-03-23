c4e2a5a6acf55756b89a614741aad556045fad5a
Christopher Sandoval <san13660@uvg.edu.gt>
2017-03-23 12:06:18 -0600

    Pruebas Unitarias

--- src/PruebasUnitarias.java
+++ src/PruebasUnitarias.java
@@ -5,8 +5,17 @@ import org.junit.Test;
 public class PruebasUnitarias {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void insertar() {
+		BinarySearchTree<Asociacion<Integer, Integer>> b = new BinarySearchTree<>();
+		b.insert(new Asociacion<Integer,Integer>(1,3));
+		assertEquals("Insercion: ", b.delete(new Asociacion<Integer, Integer>(1, 0)),true);
+	}
+	
+	@Test
+	public void buscar() {
+		BinarySearchTree<Asociacion<Integer, Integer>> b = new BinarySearchTree<>();
+		b.insert(new Asociacion<Integer,Integer>(1,3));
+		assertEquals("Insercion: ", b.find(new Asociacion<Integer, Integer>(1, 0)).value,3,0.001);
 	}
 
 }
